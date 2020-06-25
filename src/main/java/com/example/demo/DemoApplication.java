c2812c821bbf9802503444d2e69e436da83f3565
mtculberson <tannerculberson.capstone@gmail.com>
2020-06-25 17:23:08 -0400

    adding thymeleaf to all html pages

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
+
+	@RequestMapping("index")
+	String index() {
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/demo/User/UserController.java
+++ src/main/java/com/example/demo/User/UserController.java
@@ -4,18 +4,29 @@ import javax.validation.Valid;
 
 import com.example.demo.User.User;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RestController()
+@Controller()
 
 public class UserController{
 
-    @GetMapping(value="/sign-up", name="sign-up")
-    public String getSignUp() {
+    @RequestMapping("/sign-up")
+    String getSignUp() {
         return "sign-up";
+    }
+
+    @RequestMapping("/sign-in")
+    String getSignIn() {
+        return "sign-in";
+    }
+
+    @RequestMapping("/forgot")
+    String getForgot() {
+        return "forgot";
     }
 }
