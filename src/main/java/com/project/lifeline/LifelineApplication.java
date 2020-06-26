70617a4172a2f70761d0ba0c05324ae72ce0eb33
mtculberson <tannerculberson.capstone@gmail.com>
2020-06-25 23:34:15 -0400

    build

--- src/main/java/com/project/lifeline/LifelineApplication.java
+++ src/main/java/com/project/lifeline/LifelineApplication.java
@@ -2,9 +2,20 @@ package com.project.lifeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
+
+
 public class LifelineApplication {
+
+	@RequestMapping("/index")
+	String getIndex() {
+		return "index";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(LifelineApplication.class, args);
 	}
