278f3ae6e7a161198612ed0dafb14a472142573e
mtculberson <tannerculberson.capstone@gmail.com>
2020-07-11 17:08:02 -0400

    Unit tests changes

--- src/test/java/com/project/lifeline/Services/UsersServiceTest.java
+++ src/test/java/com/project/lifeline/Services/UsersServiceTest.java
@@ -4,14 +4,20 @@ import com.project.lifeline.Models.Contact;
 import com.project.lifeline.Models.RegisterUserModel;
 import com.project.lifeline.Models.Users;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.project.lifeline.Services.UsersService;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class UsersServiceTest {
 
     @Autowired
@@ -19,7 +25,6 @@ class UsersServiceTest {
 
     @Test
     void createNewUser() {
-
         RegisterUserModel register = new RegisterUserModel();
         register.setFirstName("");
         register.setLastName("");
@@ -27,10 +32,13 @@ class UsersServiceTest {
         register.setPassword("");
         register.setUsername("");
 
-        Users user = usersService.createNewUser(register);
+        Users user = new Users();
 
-        assertThat(user.getUserId()).isNull();
-
+        try {
+            user = usersService.createNewUser(register);
+        } catch (Exception ex) {
+            assertThat(user).isNull();
+        }
 
     }
 
